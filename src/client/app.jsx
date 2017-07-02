5a2790d0ebe4f086bb87917a696f977f60a4f210
Kumar Gaurav <kumar.gaurav@flyvictor.com>
2017-07-02 13:25:32 +0100

    Removed refresh component and associated

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -11,36 +11,12 @@ function Square(props) {
     )
 }
 
-function Restart(props) {
-    return (
-        <a href='#' onClick={props.onClick}>
-           Refresh
-        </a>
-    )
-}
-
 class Board extends React.Component {
-    handleRestart() {
-        console.log('clicked refresh')
-        this.setState({
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        })
-    }
-    
     renderSquare(i) {
         return (
             <Square
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
-            />
-        )
-    }
-
-    renderRestart() {
-        return (
-            <Restart
-                onClick={() => this.handleRestart()}
             />
         )
     }
@@ -111,21 +87,21 @@ class Game extends React.Component {
             status = 'Next player: ' + (this.state.xIsNext? "X" : "O")
         }
         
-    return (
-      <div className="game">
-        <div className="game-board">
-            <Board
-                squares={current.squares}
-                onClick={(i) => this.handleClick(i)}
-            />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
 }
 
 function calculateWinner(squares) {
